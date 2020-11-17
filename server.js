3eb8c7fa2dc0f9905b655fbad156a46c242fb8c3
Shirley Lee <synchrotronflux@yahoo.com>
2020-11-16 20:41:42 -0500

    Sign up and login

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-const classRouter = require('./classes/class-router');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const classRouter = require("./classes/class-router");
+const userRouter = require("./users/user-router");
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
-server.use('/classes', classRouter);
+server.use("/classes", classRouter);
+server.use("/users", userRouter);
 
 server.use((err, req, res, next) => {
-    console.log(err);
-    return res.status(500).json({
-        message: "A server error has occurred"
-    })
-})
+  console.log(err);
+  return res.status(500).json({
+    message: "A server error has occurred",
+  });
+});
 
 module.exports = server;
